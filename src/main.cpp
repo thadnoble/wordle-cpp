2c4134555f0302962d408ce60e05d330182ae337
thad <thad.noble@proton.me>
2024-09-02 00:55:37 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main() {
+    std::vector<std::string> words;
+    std::ifstream file("wordlist.txt");
+    std::string words;
+}
